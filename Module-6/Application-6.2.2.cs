a6233e0a6ecca5df44e3586086b830e31c566697
Tamer <tamer.masud@mail.ru>
2024-03-18 16:53:07 +0400

    Добавьте файлы проекта.

---
+++ Module-6/Application-6.2.2.cs
@@ -0,0 +1,30 @@
+﻿/*
+using System;
+
+namespace Module_6
+{
+    class Application_6_2_2
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+}
+*/
---
+++ Module-6/Application-6.2.8.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Module_6
+{
+    class Application_6_2_8
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Rectangle
+    {
+        public int a;
+        public int b;
+
+        public int Square()
+        {
+            return a * b;
+        }
+
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+        }
+
+        public Rectangle(int side)
+        {
+            a = side;
+            b = side;
+        }
+
+        public Rectangle(int first, int second)
+        {
+            a = first;
+            b = second;
+        }
+    }
+}
